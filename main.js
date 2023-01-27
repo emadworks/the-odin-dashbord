5c84c26430aa58ef1fe38107919dcf02616e9168
Emad Aldeen <112932679+emadworks@users.noreply.github.com>
2023-01-27 11:41:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function toggle() {
+document.getElementById('ul').style.display='flex';
+}
+
+
+// if (onclick()) {
+// 		document.getElementById('ul').style.display='flex';
+// 	} else {
+// 		document.getElementById('ul').style.display='none';
+// 	}
